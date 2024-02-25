2176d6eed33e91e5e11ec2714be8514e747c8108
mnshkv <aleksandr.menshikov@gmail.com>
2024-02-25 17:17:38 +0500

    chapter 10 override boss

--- overriding_boss.cpp
+++ overriding_boss.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
 
+class Enemy
+{
+public:
+    Enemy(int damage = 10);
+    void virtual Taunt() const;
+    void virtual Attack() const;
+
+private:
+    int m_Damage;
+};
+Enemy::Enemy(int damage) : m_Damage(damage) {}
+void Enemy::Taunt() const
+{
+    std::cout << "The enemy says he will fight you.\n";
+}
+void Enemy::Attack() const
+{
+    std::cout << "Attack! Inflicts " << m_Damage << " damage points.";
+}
+class Boss : public Enemy
+{
+    public:
+    Boss(int damage = 30);
+    void virtual Taunt() const;
+    void virtual Attack() const;
+};
+Boss::Boss(int damage) : Enemy(damage) {}
+
+void Boss::Taunt() const
