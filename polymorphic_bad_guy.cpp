8c998c6b74dfcc380b74e7e99d62f8c1f6d35692
mnshkv <aleksandr.menshikov@gmail.com>
2024-02-29 10:17:55 +0500

    chapter 10 cont

---
+++ polymorphic_bad_guy.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Enemy
+{
+public:
+    Enemy(int damage = 10);
+    virtual ~Enemy();
+    void virtual Attack() const;
+
+protected:
+    int* m_pDamage;
+};
+Enemy::Enemy(int damage)
+{
+    m_pDamage = new int(damage);
+}
+Enemy::~Enemy()
+{
+    std::cout << "In Enemy destructor, deleting m_pDamage.\n";
+    delete m_pDamage;
+    m_pDamage = 0;
+}
+void Enemy::Attack() const
+{
+    std::cout << "The enemy attacks and inmplicts " << *m_pDamage << " damage points";
+}
+class Boss : public Enemy
+{
+public:
+    Boss(int multiplier = 3);
+    virtual ~Boss();
+    void virtual Attack() const;
+
+protected:
+    int* m_pMultiplier;
+};
+Boss::Boss(int multiplier)
+{
+    m_pMultiplier = new int(multiplier);
+}
+Boss::~Boss()
+{
+    std::cout << "In Boss destructor, deleting m_pDamage.\n";
+    delete m_pMultiplier;
+    m_pMultiplier = 0;
+}
+void Boss::Attack() const
+{
+    std::cout << "A boss attacks and inflicts " << (*m_pDamage) * (*m_pMultiplier) << " damage points.\n";
+}
+int main()
+{
+    return 0;
+}
